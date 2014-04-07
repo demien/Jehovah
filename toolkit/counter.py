056db26f927b2b3212e38be1dce7fe618465a40b
demien <demienli@gmail.com>
2014-04-07 11:46:54 +0800

    init counter

--- toolkit/counter.py
+++ toolkit/counter.py
@@ -1,4 +1,4 @@
 @staticmethod
-def count_user():
+def count_user(file_path='../data_resource/t_alibaba_data.csv'):
     command = \
-        "awk 'BEGIN{FS=","}{if(!($1 in d)){d[$1] = 1}}END{for(i in d){n++} print n}' t_alibaba_data.csv"
+        "awk 'BEGIN{FS=","}{if(!($1 in d)){d[$1] = 1}}END{for(i in d){n++} print n}' %s" % file_path
